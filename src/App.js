1174ffd1020bad3f3ec23b16d4838319de080949
cheezedoodles <hougvapes@gmail.com>
2023-02-23 19:29:15 +0300

    added an endpoint for retrieving flights and page/flightsData states

--- src/App.js
+++ src/App.js
@@ -1,6 +1,15 @@
 import './App.css';
+import { useState, useCallback, useEffect } from 'react';
 
 function App() {
+
+  const [page, setPage] = useState(1)
+  const [flightsData, setFlightsData] = useState({
+    flights: [], page: 1, exists: false
+  })
+  const API_FETCH_FLIGHTS = `http://127.0.0.1:3001/api/flights?page=${page}`
+
+
   return (
     <div className="App">
       <h1>Airflights</h1>
